24722ef5d30004d506e409e52c7552cfab380dba
Merab Aghniashvili <merab.aghniashvili@flatrocktech.com>
2023-08-06 09:48:52 +0400

    endpoints changed back

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,7 +15,7 @@ export const endpoints = {
 }
 
 
-const url = `https://fmg.mark4.ge/api/`;
+const url = `http://app.mark4.ge:8520/api/`;
 
 const axiosAutorization = axios.create({
   baseURL: url,
@@ -34,7 +34,7 @@ const axiosInstance = axios.create({
 });
 
 const axiosOperationInstance = axios.create({
-  baseURL: "https://fmg.mark4.ge/",
+  baseURL: "http://app.mark4.ge:8520/",
   headers: {
     'Accept': 'application/json',
     "Accept-Encoding": "gzip, deflate",
