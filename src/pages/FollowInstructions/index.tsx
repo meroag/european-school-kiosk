cecc27d87cbaa84373127509241fa7873bbdda6b
kezhera <>
2023-09-12 17:20:23 +0400

    fix: remove print

--- src/pages/FollowInstructions/index.tsx
+++ src/pages/FollowInstructions/index.tsx
@@ -14,35 +14,11 @@ const FollowInstructions = () => {
   const salaroId = useSettingStore(state => state.selectedSalaroId)
   const finalizeOrder = useCartStore(state => state.finalizeOrder)
 
-
-  // const print = (text: any) => {
-  //   var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
-
-  //   if(WinPrint){
-  //     WinPrint.document.write(text);
-  //     WinPrint.document.close();
-  //     WinPrint.focus();
-  //     WinPrint.print();
-  //     WinPrint.close();
-  //   }
-  // }
-  const printEthernalPrinter = async (text: string) => {
-    // @ts-ignore: Unreachable code error
-    const port = await window.navigator.serial.requestPort();
-    await port.open({ baudRate: 9600 });
-    
-    const writer = port.writable?.getWriter();
-    if (writer != null) {
-      await writer.write(text);
-      writer.releaseLock();
-    }
-  }
-  
   const saleProduct = async () => {
     try {
       const orderId = await finalizeOrder()
 
-      const resp: any = await axiosOperationInstance.post(
+      await axiosOperationInstance.post(
         endpoints.PayTerminal,
         {
           Amount: total.price,
@@ -53,14 +29,10 @@ const FollowInstructions = () => {
 
       await payOrders()
 
-      // print(resp.data)
-      printEthernalPrinter(resp.data)
       navigate("/payment-successful")
 
     } catch (err: any) {
       console.log(err)
-      // print(err.response.data)
-      // printEthernalPrinter(err.response.data)
       navigate("/payment-declined")
     }
   } 
